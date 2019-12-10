1722e889beb58ddf66cb15d46d67a5e65d7c8230
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-12-10 13:49:42 -0700

    added knexfile.js, migrations folder and db3 file

---
+++ cars/cars-router.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const knex = require("knex");
+
+const db = knex({
+  client: "sqlite3",
+  connection: {
+    filename: "./data/car-dealer.db3"
+  },
+  useNullAsDefault: true
+});
+const router = express.Router()
+
+
+// router.get('/', (req,res) => {
+//     db('cars')
+// })
+
+module.exports = router;
