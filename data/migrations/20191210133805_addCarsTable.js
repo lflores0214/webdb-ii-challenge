a7a06c2480ca0fa7a35ab9356787f79f8549319b
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-12-10 14:02:04 -0700

    added cars table schema

--- data/migrations/20191210133805_addCarsTable.js
+++ data/migrations/20191210133805_addCarsTable.js
@@ -1,8 +1,25 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("cars", tbl => {
+    tbl.increments();
+
+    tbl
+      .integer("VIN")
+      .notNullable()
+      .unique()
+      .index();
+
+    tbl.string("make", 20).notNullable();
+
+    tbl.string("model", 20).notNullable();
+
+    tbl.integer("mileage").notNullable();
+
+    tbl.string("transmission_type", 20);
+
+    tbl.string("title_status", 10);
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("cars");
 };
