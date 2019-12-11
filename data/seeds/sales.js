2757f051eb01b3ab2228dc9852aa41469388ad95
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-12-10 17:33:10 -0700

    added sales migration and sales seed

--- data/seeds/sales.js
+++ data/seeds/sales.js
@@ -1,13 +1,25 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("table_name")
+  return knex("sales")
     .truncate()
     .then(function() {
       // Inserts seed entries
-      return knex("table_name").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" }
+      return knex("sales").insert([
+        {
+          car_id: 1,
+          sold: true,
+          seller: "Mike W.",
+          buyer: "Sully",
+          sold_for: 20000
+        },
+
+        {
+          car_id: 2,
+          sold: true,
+          seller: "John Doe",
+          buyer: "Jane Doe",
+          sold_for: 19800
+        }
       ]);
     });
 };
